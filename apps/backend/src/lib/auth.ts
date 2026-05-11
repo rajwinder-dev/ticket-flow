b32d347b9b8ae00b2cd8b0092b61e88884f55921
rajwindersxxx <rajwindersxxx@gmail.com>
2026-05-12 03:14:10 +0530

    working on forget password

--- apps/backend/src/lib/auth.ts
+++ apps/backend/src/lib/auth.ts
@@ -2,6 +2,8 @@ import { betterAuth } from "better-auth";
 
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "../core/utils/prismaClient.js";
+import { EmailService } from "../modules/email/email.service.js";
+import ForgotPasswordEmail from "../templates/emails/ForgotPasswordEmail.js";
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
@@ -13,5 +15,12 @@ export const auth = betterAuth({
   },
   emailAndPassword: {
     enabled: true,
+    sendResetPassword: async ({ user, url }) => {
+      await EmailService.sendSystemEmail({
+        to: user.email,
+        subject: "Reset your password",
+        jsx: ForgotPasswordEmail({ userName: user.name!, resetLink: url }),
+      });
+    },
   },
 });
