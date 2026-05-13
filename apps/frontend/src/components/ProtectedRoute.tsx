6854f8387e3c85e5c24f166aa96fe8278d74a26c
rajwindersxxx <rajwindersxxx@gmail.com>
2026-05-13 17:39:08 +0530

    fix login issue , stall state cause route protect

--- apps/frontend/src/components/ProtectedRoute.tsx
+++ apps/frontend/src/components/ProtectedRoute.tsx
@@ -1,21 +1,60 @@
-import { Navigate, Outlet } from "react-router";
+import { Outlet, useLocation, useNavigate } from "react-router";
 import { Spinner } from "./ui/spinner";
 import { useMembersStore } from "@/features/members/store";
 import { authClient } from "@/lib/auth-client";
+import { useEffect } from "react";
+
 const ProtectedRoute = () => {
   const { data: session, isPending } = authClient.useSession();
+
   const { tokenEmail, inviteToken } = useMembersStore();
-  if (isPending)
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (isPending) return;
+
+    // unauthenticated
+    if (!session) {
+      navigate("/login", {
+        replace: true,
+        state: {
+          from: location.pathname,
+        },
+      });
+
+      return;
+    }
+
+    // invite redirect
+    if (
+      inviteToken &&
+      tokenEmail === session.user.email
+    ) {
+      navigate(`/invite-user/${inviteToken}`, {
+        replace: true,
+      });
+    }
+  }, [
+    session,
+    isPending,
+    navigate,
+    location,
+    inviteToken,
+    tokenEmail,
+  ]);
+
+  if (isPending || session === undefined) {
     return (
       <div className="flex h-screen w-screen items-center justify-center">
         <Spinner className="size-8" />
       </div>
     );
-  if (!session) return <Navigate to="/login" />;
+  }
 
-  // check if there is any invite pending
-  if (inviteToken && tokenEmail === session?.user.email)
-    return <Navigate to={`/invite-user/${inviteToken}`} />;
+  if (!session) return null;
+
   return <Outlet />;
 };
 
