1bf249d759ca375f81c0cb9717c00e37357e7248
rajwindersxxx <rajwindersxxx@gmail.com>
2026-07-26 19:18:17 +0530

    setup nginx for socket.io

--- app/api/src/config/env.ts
+++ app/api/src/config/env.ts
@@ -4,8 +4,6 @@ dotenv.config();
 
 export const env = {
   encryptionKey: process.env.ENCRYPTION_KEY,
-  wss: process.env.WSS,
-  coreURL: process.env.BETTER_AUTH_URL,
   betterAuthUrl: process.env.BETTER_AUTH_URL,
   betterAuthSecret: process.env.BETTER_AUTH_SECRET,
   refreshSecret: process.env.REFRESH_SECRET,
